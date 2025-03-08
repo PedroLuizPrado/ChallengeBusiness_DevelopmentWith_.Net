f22dad71a91e25bdeb464c385fe869849dd5e112
PedroLuizPrado <142911409+PedroLuizPrado@users.noreply.github.com>
2025-03-07 21:19:11 -0300

    Sprint03

---
+++ Sprint03/OdontoPrevSprint03/Models/User.cs
@@ -0,0 +1,28 @@
+﻿namespace OdontoPrevSprint03.Models
+{
+    /// <summary>
+    /// Representa um usuário (paciente) no sistema.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Identificador único do usuário.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome completo do usuário.
+        /// </summary>
+        public string? Nome { get; set; }
+        /// <summary>
+        /// Data de aniversário do usuário.
+        /// </summary>
+        public DateTime DataAniversario { get; set; }
+
+
+        /// <summary>
+        /// Endereço de e-mail do usuário.
+        /// </summary>
+        public string? Email { get; set; }
+    }
+}
---
+++ Sprint03/OdontoPrevSprint03/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using OdontoPrevSprint03.Models;
+
+namespace OdontoPrevSprint03.Services
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetAllUsers();
+        Task<User> GetUserById(int id);
+        Task AddUser(User user);
+        Task UpdateUser(User user);
+        Task DeleteUser(int id);
+    }
+}
